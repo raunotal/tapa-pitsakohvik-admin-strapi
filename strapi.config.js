a2a9c103413d64f2b01da4dfe9a3b00667cebc6e
Rauno Tali <rauno@pats.ee>
2023-04-06 21:53:31 +0300

    fix cwd in strapi pm2 config

--- strapi.config.js
+++ strapi.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "strapi",
       script: "npm",
       args: "start",
-      cwd: process.env.HOME + "/strapi",
+      cwd: process.env.HOME + "/strapi/source",
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "production",
