093b2e4596543c8fb27e3c2fdbf5ed15e2c21a8b
Rauno Tali <rauno@pats.ee>
2023-04-07 21:29:15 +0300

    update strapi pm2 config

--- strapi.config.js
+++ strapi.config.js
@@ -5,7 +5,11 @@ module.exports = {
       script: "npm",
       args: "start",
       max_memory_restart: "512M",
+      cwd: "/data01/virt53516/strapi/source/",
       env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
         NODE_ENV: "production",
       },
     },
