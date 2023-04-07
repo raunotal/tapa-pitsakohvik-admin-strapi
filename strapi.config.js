9414f8757d755265776904418bf7f994ba46f087
Rauno Tali <rauno@pats.ee>
2023-04-07 19:40:51 +0300

    update strapi conf file

--- strapi.config.js
+++ strapi.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "strapi",
       script: "npm",
       args: "start",
-      cwd: process.env.HOME + "/strapi/source",
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "production",
